265a472a186eeb3083ee872a142970c600351d34
Saeed <saeed.adelpour99@gmail.com>
2025-10-08 23:52:29 +0330

    fix(028): clean code

--- m_028.py
+++ m_028.py
@@ -87,7 +87,7 @@ class Graph:
             word_node.neighbors[new_word] = new_word_node
             new_word_node.neighbors[word] = word_node
 
-    def create_cluster(self, word, level=0, checklist=set()):
+    def create_cluster(self, word, checklist=set()):
         checklist.add(word)
 
         for new_word in get_new_words(word):
@@ -95,69 +95,35 @@ class Graph:
             self.check_exists_in_tails(word, new_word)
 
             if not self.is_word_checked(new_word, checklist):
-                self.create_cluster(new_word, level + 1, checklist)
-        # return level
+                self.create_cluster(new_word, checklist)
 
     def is_word_checked(self, new_word, checklist=[], directions={}):
         return new_word in checklist
 
-    def find_best_route(self, word1, word2, routes=[], checklist=set(), level=0):
+    def find_best_route(self, word1, word2, routes=[], checklist=set()):
         if word1 in checklist:
             return routes
 
-        # print("*" * 40, word1, word2)
-        # print({"routes": routes})
-        # if level > 1:
-        #     return routes
         node1 = self.nodes[word1]
         node2 = self.nodes[word2]
 
         if node2.data in node1.neighbors:
-            print("end", word1, word2)
-            self.add_to_routes(word1, word2, word2, routes, checklist)
+            self.add_to_routes(word1, word2, word2, routes)
             return routes
 
         for _n1 in node1.neighbors.values():
-            # print(
-            #     {
-            #         "_n1": _n1,
-            #         "node1": node1,
-            #         "routes": routes,
-            #         "checklist": checklist,
-            #     }
-            # )
-            # input("continue?")
 
-            self.add_to_routes(node1.data, _n1.data, word2, routes, checklist)
+            self.add_to_routes(node1.data, _n1.data, word2, routes)
 
         checklist.add(word1)
 
         for _n1 in node1.neighbors.values():
 
-            self.find_best_route(_n1.data, word2, routes, checklist, level + 1)
+            self.find_best_route(_n1.data, word2, routes, checklist)
             checklist.add(_n1.data)
-            # print({"routes": routes, "checklist": checklist})
         return routes
 
-    def add_to_routes(self, word1, word2, dest_word, routes, checklist, do_print=False):
-        if word1 in (
-            # "sta",
-            "reg",
-        ) or word2 in ("reg",):
-            do_print = True
-
-        # do_print = True
-        # do_print = False
-
-        if do_print:
-            log_job = self.write
-        else:
-            log_job = lambda *args, **kwargs: ...
-
-        # if word1 == "stu":
-        #     self.write(routes, "routes.json")
-
-        # log_job("-" * 40, word1, word2)
+    def add_to_routes(self, word1, word2, dest_word, routes):
         found_similar_routes_idx = [
             [i, r]
             for i, r in enumerate(routes)
@@ -170,60 +136,18 @@ class Graph:
         x = self.get_min(found_similar_routes)
         y = self.get_min(routes_with_last_node)
         last_node_route_is_smaller = x is not None and y is not None and x > y
-        # last_node_route_is_smaller = False
-        # print(last_node_route_is_smaller)
         should_add_route = (not found_similar_routes) or last_node_route_is_smaller
-        log_job(
-            {
-                "found_similar_routes": found_similar_routes,
-                "routes_with_last_node": routes_with_last_node,
-                "should_add_route": should_add_route,
-                "x": x,
-                "y": y,
-                "routes": routes,
-            },
-            "{}_state_found_similar_routes_{}_{}.json".format(
-                time.time(), word1, word2
-            ),
-        )
-
         if last_node_route_is_smaller:
             for i, _ in found_similar_routes_idx:
                 routes[i] = []
 
-        # if last_node_route_is_smaller and found_similar_routes:
-
-        log_job(
-            {
-                # "found_similar_routes": found_similar_routes,
-                # "routes_with_last_node": routes_with_last_node,
-                "should_add_route": should_add_route,
-                # "routes": routes,
-            },
-            "{}_found_similar_routes_{}_{}.json".format(time.time(), word1, word2),
-        )
         if should_add_route:
-            # log_job(
-            #     {
-            #         "step": "before",
-            #         "len(routes)": len(routes),
-            #         "routes_with_last_node": routes_with_last_node,
-            #         "routes": routes,
-            #     },
-            #     "{}_{}.json".format(word1, word2),
-            # )
 
             if routes_with_last_node:
                 for r in routes_with_last_node:
                     routes.append([*r, word2])
             else:
                 routes.append([word1, word2])
-
-            # if routes_with_last_node:
-            #     for r in routes_with_last_node:
-            #         r.append(word2)
-            # else:
-            #     routes.append([word1, word2])
 
     def get_min(self, routes):
         if not routes:
@@ -235,20 +159,15 @@ class Graph:
             json.dump(data, fp, ensure_ascii=False, indent=2)
 
 
-# init
-word1 = "pig"
-word2 = "sty"
-routes = []
-checklist = []
-
-
 def main():
     word1 = "pig"
     word2 = "sty"
     self = Graph()
 
-    self.create_cluster(word1)
-    print(self.tails)
+    self.create_cluster(word1, set())
+    x = self.find_best_route(word2, word1, [], set())
+    routes = sorted([r for r in x if r and r[-1] == word1], key=len, reverse=True)
+    print(routes)
 
 
 if __name__ == "__main__":
