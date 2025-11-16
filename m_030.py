d4e22110999b0a7b86acc9b0b19bc8cd68de8a1b
Saeed <saeed.adelpour99@gmail.com>
2025-11-16 16:37:26 +0330

    fix(030): enhance and finalize

--- m_030.py
+++ m_030.py
@@ -1,10 +1,9 @@
-import json
 import operator
 
 from rich import print
 
 
-def find_next_positions_old(position, max_x, max_y, path):
+def find_next_positions(position, max_x, max_y, route):
     x = position[0]
     y = position[1]
 
@@ -18,55 +17,44 @@ def find_next_positions_old(position, max_x, max_y, path):
                 _y = y_op(y, n2)
                 if 0 <= _x < max_x and 0 <= _y < max_y:
                     p = (_x, _y)
-                    if p not in path:
+
+                    if p not in route:
                         next_positions.append(p)
+
     return next_positions
-
-
-DELTAS = ((-2, -1), (-2, 1), (2, -1), (2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2))
-
-
-def find_next_positions(position, max_x, max_y, path):
-    out = []
-    for dx, dy in DELTAS:
-        cx, cy = position[0] + dx, position[1] + dy
-        if 0 <= cx < max_x and 0 <= cy < max_y and (cx, cy) not in path:
-            out.append((cx, cy))
-    return out
 
 
 def knight_moves(max_x, max_y):
     start = (0, 0)
-    todo_list = [(start, {start: None})]
+    todo_list = [
+        [
+            start,
+            [start],
+        ],
+    ]
     goal = max_x * max_y
 
     while todo_list:
 
-        position, path = todo_list.pop()
-        _max = len(path)
-        print("max", _max)
-        if len(path) == goal:
-            return path
+        position, route = todo_list.pop()
+        if len(route) == goal:
+            return route
 
-        next_positions = find_next_positions(position, max_x, max_y, path)
+        next_positions = find_next_positions(position, max_x, max_y, route)
         next_positions.sort(
-            key=lambda c: len(find_next_positions(c, max_x, max_y, path)), reverse=True
+            key=lambda _p: len(find_next_positions(_p, max_x, max_y, route)),
+            reverse=True,
         )
+
         for next_position in next_positions:
-            path_copy = path.copy()
-            path_copy[next_position] = None
-            todo_list.append((next_position, path_copy))
+            todo_list.append([next_position, [*route, next_position]])
 
     return None
 
-    raise Exception("can't found any routes")
-
 
 if __name__ == "__main__":
-    routes = knight_moves(7, 7)
-    with open("routes.json", "w") as fp:
-        json.dump(routes, fp, indent=2)
-    # print(routes)
+    routes = knight_moves(8, 8)
+    print(routes)
 
     # next_positions = find_next_nodes((4, 4), 8, 8)
     # print(next_positions)
