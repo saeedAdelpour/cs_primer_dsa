54713df9f9c348eb03e14e37a6bfd7d86f3552eb
Saeed <saeed.adelpour99@gmail.com>
2026-03-22 20:43:43 +0400

    feat(0031): clean code

--- m_031.py
+++ m_031.py
@@ -1,16 +1,12 @@
 import ipdb
 import sys
-import operator
 from itertools import product
 
 import numpy as np
 from rich import print
 
-ops = (operator.add, operator.sub)
-deltas = (1,)
 
-
-def find_next_positions(position, _map, route, destination, checklist):
+def find_next_positions(position, _map, route):
     max_x, max_y = _map.shape
 
     x = position[0]
@@ -21,12 +17,7 @@ def find_next_positions(position, _map, route, destination, checklist):
 
     out = []
     for p in positions:
-        if (
-            0 <= p[0] < max_x
-            and 0 <= p[1] < max_y
-            and p not in route
-            # and p not in checklist
-        ):
+        if 0 <= p[0] < max_x and 0 <= p[1] < max_y and p not in route:
             char = _map[p[0], p[1]]
             out.append((p, char))
     out.sort(key=lambda x: cost_map[x[1]])
@@ -60,72 +51,37 @@ def find_best_route(_map):
     final_routes = []
     max_todo_length = 0
     while todo_list:
-        # print("*" * 40)
         max_todo_length = max(max_todo_length, len(todo_list))
-        # if max_todo_length > 600:
-        #     ipdb.set_trace()
-
-        # print(len(todo_list))
-        # print(len(checklist))
-        # print(todo_list)
         position, (route, length) = todo_list.pop(0)
-        # print(position)
-        next_positions = find_next_positions(
-            position, _map, route, destination, checklist
-        )
-        # input("continue...")
-
-        # if position in checklist:
-        #     continue
-        found_destination = False
-
+        next_positions = find_next_positions(position, _map, route)
         for next_position, char in next_positions:
             new_route = (*route, next_position)
             new_length = length + cost_map[char]
 
             if next_position == destination:
-                # print("found destination, continue...")
                 final_routes.append((new_route, new_length))
-                # return final_routes
-                found_destination = True
                 break
-                # continue
-            # if found_destination:
-            #     checklist.add(next_position)
-            #     continue
 
             found_better_in_todo_list = can_found_better_in_todo_list(
-                next_position, todo_list, new_length, destination
+                next_position, todo_list, new_length
             )
-            # found_better_in_todo_list = False
 
-            if next_position not in checklist:
-                if not found_better_in_todo_list:
-                    todo_list.append((next_position, (new_route, new_length)))
-                    todo_list = remove_not_better_from_todo_list(
-                        next_position, new_length, todo_list
-                    )
-            # todo_list.append((next_position, (new_route, new_length)))
-
-        # if found_destination:
-        #     for next_position, char in next_positions:
-        #         checklist.add(next_position)
+            if next_position not in checklist and not found_better_in_todo_list:
+                todo_list.append((next_position, (new_route, new_length)))
+                todo_list = remove_not_better_from_todo_list(
+                    next_position, new_length, todo_list
+                )
 
         checklist.add(position)
 
     return final_routes, max_todo_length
 
 
-def can_found_better_in_todo_list(position, todo_list, length, destination):
+def can_found_better_in_todo_list(position, todo_list, length):
     return any(
         _position
         for _position, (_route, _length) in todo_list
-        if (
-            True
-            and _position == position
-            # and _position == destination
-            and _length <= length
-        )
+        if _position == position and _length <= length
     )
 
 
@@ -150,8 +106,6 @@ def remove_not_better_from_todo_list(position, length, todo_list):
 
 def debug():
     path = sys.argv[1]
-    # path = "m_031_map_test"
-    # path = "m_031_map_test_validity"
     with open(path) as fp:
         _map = fp.readlines()
     _map = np.array([list(x[:-1]) for x in _map])
@@ -172,6 +126,7 @@ def test():
 
     print("start test")
     for path, expect_length, expect_max_todo_length in path_length_pairs:
+        print(f"test: {path}")
         with open(path) as fp:
             _map = fp.readlines()
         _map = np.array([list(x[:-1]) for x in _map])
