97c48f92b416279baf34dcc7cb7f682775b93287
Saeed <saeed.adelpour99@gmail.com>
2026-04-13 00:46:54 +0400

    feat(m_036): house rubber problem

---
+++ m_036.py
@@ -0,0 +1,45 @@
+import enum
+from rich import print
+
+
+def choose(array, i=0, checking=[]):
+    """
+    this function takes a list of numbers and returns maximum sum of numbers
+        that are not adjacent together
+    for example:
+        array = [4, 7, 8, 5, 3]
+        then the numbers are: {4, 8, 3}, which sum = 15
+    """
+    x = [_f for f in [(k - 1, k, k + 1) for k in checking] for _f in f]
+
+    possible_idxs = [j for j in range(len(array)) if j not in (i - 1, i, i + 1, *x)]
+    if not possible_idxs:
+        idxs = [i] + checking
+        return sum([array[i] for i in idxs]), idxs
+    res = []
+    for _i in possible_idxs:
+        out = choose(array, i, [*checking, _i])
+
+        if isinstance(out, list):
+            for _o in out:
+                res.append(_o)
+        else:
+            res.append(out)
+    return res
+
+
+if __name__ == "__main__":
+
+    for test_idx, (array, expect_sum, expect_idx) in enumerate(
+        [
+            ([4, 7, 8, 5, 3], 15, [0, 2, 4]),
+            ([3, 10, 1, 2, 20], 30, [1, 4]),
+            ([3, 10, 8, 2, 20], 31, [0, 2, 4]),
+        ]
+    ):
+        out = [j for i in range(len(array)) for j in choose(array, i)]
+        out = sorted(out, key=lambda x: x[0], reverse=True)
+        _sum, _idx = out[0]
+        assert expect_sum == _sum, (test_idx, _sum)
+        assert expect_idx == _idx, (test_idx, _idx)
+    print("ok")
