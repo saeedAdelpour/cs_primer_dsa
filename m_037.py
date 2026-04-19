2fd28241bf0ac116934a1e0576ee549cbef1b9ad
Saeed <saeed.adelpour99@gmail.com>
2026-04-19 17:41:13 +0400

    fix(0036/prefect_square): pass all tests and use memoization

--- m_037.py
+++ m_037.py
@@ -1,21 +1,45 @@
 import math
+from rich import print
 
 
-def perfect_square(n, count=0):
-    if n == 0:
-        return count
-    max_near = int(math.sqrt(n))
-    max_near = max_near**2
-    return perfect_square(n - max_near, count + 1)
+def perfect_square(n):
+    memo = {}
+
+    def __perfect_square(n, count=0, nums=tuple()):
+        if n in memo:
+            return memo[n]
+
+        if n == 0:
+            return count, nums
+
+        max_near = int(math.sqrt(n))
+        squares = [i**2 for i in range(max_near, 1, -1)]
+        if not squares:
+            return __perfect_square(n - 1, count + 1, (*nums, 1))
+
+        res = min(
+            (__perfect_square(n - sq, count + 1, (*nums, sq)) for sq in squares),
+            key=lambda x: x[0],
+        )
+        memo[n] = res
+        return res
+
+    x = __perfect_square(n)
+    return x
 
 
 if __name__ == "__main__":
     for n, nums, count in (
         (7, (4, 1, 1, 1), 4),
         (14, (9, 4, 1), 3),
-        (23, (16, 4, 1, 1, 1), 5),
-        # (23, (9, 9, 4, 1), 4),
+        (23, (9, 9, 4, 1), 4),
+        (35, (25, 9, 1), 3),
+        (54, (49, 4, 1), 3),
+        (4343, (4225, 100, 9, 9), 4),
+        (30001, (29929, 36, 36), 3),
+        (40101, (40000, 100, 1), 3),
     ):
-        ps = perfect_square(n)
-        assert ps == count, (ps, count)
+        _count, _nums = perfect_square(n)
+        assert _count == count, _count
+        assert (_nums) == (nums), _nums
     print("ok")
